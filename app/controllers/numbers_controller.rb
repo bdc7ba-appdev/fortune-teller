e3266f447df4c146d64d7cf8c57ab81b975573ed
bdc7ba <bcrozier@chicagobooth.edu>
2022-10-22 18:57:01 +0000

    Aries working

--- app/controllers/numbers_controller.rb
+++ app/controllers/numbers_controller.rb
@@ -1,13 +1,17 @@
 class NumbersController < ApplicationController
   def winners
-    @zebra = Array.new
+    @zebra = Array.new 
 
     5.times do
       giraffe = rand(1...100)
       
       @zebra.push(giraffe)
     end
-
+    
+    # @zebra = [42, 65, 13, 22,1] all instance variables defined in this template are rendered
     render({ :template => "lottery_stuff/woohoo.html.erb"})
   end 
+
+  def losers
+    @unlucky_numbers = Array.new
 end
