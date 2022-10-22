4f5fb28b5900faee4b5ded5dadae627915824d66
bdc7ba <bcrozier@chicagobooth.edu>
2022-10-22 19:37:27 +0000

    horoscopes done

--- config/routes.rb
+++ config/routes.rb
@@ -26,13 +26,13 @@ Rails.application.routes.draw do
   get("/zodiacs/virgo", { :controller => "earth", :action => "maiden" })
   get("/zodiacs/capricorn", { :controller => "earth", :action => "goat" })
   
-  # get("/zodiacs/gemini", { :controller => "air", :action => "twins" })
-  # get(" /zodiacs/libra", { :controller => "air", :action => "scales" })
-  # get ("/zodiacs/aquarius", { :controller => "air", :action => "waterbearer" })
+  get("/zodiacs/gemini", { :controller => "air", :action => "twins" })
+  get("/zodiacs/libra", { :controller => "air", :action => "scales" })
+  get("/zodiacs/aquarius", { :controller => "air", :action => "waterbearer" })
   
-  # get("/zodiacs/cancer " { :controller => "earth" :action => "crab" })
-  # get("/zodiac/ scorpio", { :controller => "Water", :action => "scorpion" })
-  # get("/zodiacs/pisces", { :controller => "water", :action => "fish " })
+  get("/zodiacs/cancer", { :controller => "waterrrr", :action => "crab" })
+  get("/zodiacs/scorpio", { :controller => "waterrrr", :action => "scorpion" })
+  get("/zodiacs/pisces", { :controller => "waterrrr", :action => "fish" })
 
   # PART 3: MORE R→C→A→V PRACTICE
   # ==========================
@@ -49,6 +49,7 @@ Rails.application.routes.draw do
   #  - Write the Ruby to make it dynamic, and work properly.
 
   # /roll/1/6
+  get("roll/1/6")
   # /roll/2/6
   # /roll/3/6
   # /roll/4/6
